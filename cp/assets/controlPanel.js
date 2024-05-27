06ebd4010a8dcd9b0d54f29a24a77ee3780d24b8
kolya5544 <kolya@iktm.me>
2024-05-28 00:53:04 +0300

    Accounts integration WIP

--- cp/assets/controlPanel.js
+++ cp/assets/controlPanel.js
@@ -28,6 +28,26 @@ function populateUserData(callback) {
     });
 }
 
+function populateAccountData(callback) {
+    $.ajax({
+        type: 'GET',
+        url: APImethod("/api/listAccounts"),
+        beforeSend: function (xhr) { setTokenXHR(xhr, token); },
+        success: function (data) {
+            if (ProcessDataForErrors(data)) {
+                console.log(data);
+                window.location = "login.html";
+                throw new Error("Not authed");
+            }
+
+            console.log(data);
+            callback(data);
+        },
+        contentType: "application/json",
+        dataType: 'json'
+    });
+}
+
 /*$("#tokenVal").text(`Ваш токен: ${getToken().substring(0, 99)}...`);*/
 
 function loadPrimaryInterface(data) {
@@ -39,4 +59,29 @@ function loadPrimaryInterface(data) {
     $("#welcomeText").text(`Здравствуйте, ${data.FirstName}!`);
 }
 
+function calculateTotalForCurrency(accounts, currencyIndex) {
+    let thisAccount = accounts.filter((z) => z.AccountCurrency == currencyIndex);
+    if (thisAccount.length == 0) return 0;
+    let sum = 0;
+    thisAccount.forEach((z) => sum += z.Balance);
+    return sum;
+}
+
+function loadAccountInterface(data) {
+    // load account information into appropriate fields
+    // calculate totals
+    var currencies = [ ["RUB", "₽"],
+                       ["USD", "$"],
+                       ["EUR", "€"] ];
+    
+    currencies.forEach(element => {
+        let signFull = element[0];
+        let signShort = element[1];
+
+        let total = calculateTotalForCurrency(data.Accounts, currencies.indexOf(element));
+        $(`#balanceInfo_${signFull}`).text(`${(total / 100).toFixed(2)} ${signShort}`);
+    });
+}
+
 populateUserData(loadPrimaryInterface);
+populateAccountData(loadAccountInterface);
